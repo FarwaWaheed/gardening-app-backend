1a3e0ec46cc2598b19af1dd686b3d9a53e70c7ec
umemaahmed009 <umemaahmed009@gmail.com>
2025-03-28 14:22:14 +0500

    mongo client code replaced with mongoose connect because the apis were not working

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const express = require('express');
 const port = 5000;
 const router = express.Router();
@@ -19,31 +19,9 @@ app.listen(port, ()=>{
 
 const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }
-  catch(e){
-    console.log(e);
-  } 
-  finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run();
+mongoose.connect(uri)
+    .then(() => console.log("Connected to MongoDB ✅"))
+    .catch(err => console.error("MongoDB connection error:", err));
 // run().catch(console.dir);
 //routes
 app.use("/user",user);
