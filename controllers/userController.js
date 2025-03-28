90770cc9a7ccc59e08e2f4907f5af12048625052
umemaahmed009 <umemaahmed009@gmail.com>
2025-03-28 14:22:45 +0500

    sign up and sign in functions working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,44 @@
 const User = require('../models/user');
+/*
+-Encrypt Password with bcrypt
+-Add Access Token functionality(optional)
+ */
 
     async function signIn(req,res){
-
+        try{
+            const{email, password} = req.body;
+            const user = await User.findOne({email});
+            if(user){
+                console.log(user);
+                if(user.password === password){
+                    return res.status(200).json({
+                        message: "User logged in successfully"
+                    })
+                }
+                else{
+                    return res.status(200).json({
+                        message: "The password is incorrect"
+                    })
+                }
+            }
+            else{
+                return res.status(400).json({
+                    message: "Please enter correct email"
+                })
+            }
+        }
+        catch(e){
+            return res.json({
+                message: e.message
+            })
+        }
     }
     async function signUp(req,res){
         try{
             const {email,name, password,role} = req.body;
-            if(await User.findOne({email:email})) {
-                res.status(409).json({
+            const existingUser = await User.findOne({email});
+            if(existingUser) {
+                return res.status(409).json({
                     message:"User already exists with this email",
                 });
             }
@@ -19,13 +50,23 @@ const User = require('../models/user');
             })
             //Saving the user in the database
             await user.save();
+            res.status(201).json({
+                message: "User registered successfully"
+            })
 
         }
         catch (e) {
-
+            return res.json({
+                message: e.message
+            });
         }
     }
     async function updateUser(req, res){
+        try{
 
+        }
+        catch(e){
+
+        }
     }
 module.exports = { signIn, signUp, updateUser };
