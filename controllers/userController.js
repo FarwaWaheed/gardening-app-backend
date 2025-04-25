72110a6148ab64551d9724e4538e160f6383fa61
umemaahmed009 <umemaahmed009@gmail.com>
2025-04-25 22:32:45 +0500

    passing user object in login api

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,15 +9,16 @@ const User = require('../models/user');
             const{email, password} = req.body;
             const user = await User.findOne({email});
             if(user){
-                console.log(user);
                 if(user.password === password){
                     return res.status(200).json({
-                        message: "User logged in successfully"
+                        message: "User logged in successfully",
+                        user: user,
                     })
                 }
                 else{
                     return res.status(200).json({
-                        message: "The password is incorrect"
+                        message: "The password is incorrect",
+
                     })
                 }
             }
