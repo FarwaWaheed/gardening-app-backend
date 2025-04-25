3d224f27259546ee341fc3bd7c652912273bc9fa
umemaahmed009 <umemaahmed009@gmail.com>
2025-04-25 14:02:17 +0500

    routes added for garden apis

---
+++ routes/gardenRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const {
+    addPlant,
+    getAllPlants,
+    deletePlant
+} = require("../controllers/gardenController");
+
+const router = express.Router();
+
+router.post('/addPlant/:userId', addPlant);
+router.get('/getPlants/all/:userId', getAllPlants);
+router.delete('/deletePlant/:userId', deletePlant);
