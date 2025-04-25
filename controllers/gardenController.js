0e624a37efb4e9bb1993079da9addf92c7575a2a
umemaahmed009 <umemaahmed009@gmail.com>
2025-04-25 13:54:47 +0500

    garden controller apis created

---
+++ controllers/gardenController.js
@@ -0,0 +1,45 @@
+const garden = require('../models/userPlants');
+const Plant = require("../models/plant");
+
+const addPlant = async (req,res) => {
+    try{
+        const userId = req.params.userId;
+        const plantId = req.body.plantId;
+
+        const userPlant = new garden(userId ,plantId);
+        const savedPlant = await userPlant.save();
+        return res.status(201).json({ message: 'Plant added successfully', savedPlant});
+    }
+    catch (error) {
+        return res.status(500).json({ message: 'Error adding plant', error: error.message });
+    }
+}
+
+const getAllPlants = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const plants = await garden.find({userId: userId});
+        return res.status(200).json(plants);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching plants', error: error.message });
+    }
+};
+
+
+const deletePlant = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const plantId = req.body.plantId;
+        const plant = await garden.find({userId: userId, plantId: plantId});
+        const deleted = await garden.findByIdAndDelete(plant.id);
+        if (!deleted) return res.status(404).json({ message: 'Plant not found' });
+        res.status(200).json({ message: 'Plant deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting plant', error: error.message });
+    }
+};
+module.exports = {
+    addPlant,
+    getAllPlants,
+    deletePlant
+}
