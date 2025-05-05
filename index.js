17ab050080f8f721c83cdf58985152f9498a4505
umemaahmed009 <umemaahmed009@gmail.com>
2025-05-05 06:03:33 +0500

    code fixed for displaying uploaded images

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded())
 // parse application/json
 app.use(bodyParser.json())
 // app.use('/uploads', express.static('uploads'));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));
 
 
 const uri = process.env.MONGODB_URI;
